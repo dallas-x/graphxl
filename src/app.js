38c39d9d5e0fb2b6e4c12e6a6b90be51bd7f331e
Dallas Baker <dallas.baker@sheldyn.io>
2021-12-07 04:46:22 -0800

    :tada: Begin project

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from 'apollo-server';
+import Debug from 'debug';
+
+const log = Debug('graph:app');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  log(`🚀 Server ready at ${url}`);
+});
